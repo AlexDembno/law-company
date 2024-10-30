96e1b39230fe103b4f7082a89c440a62102d8897
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2024-10-30 14:27:14 +0100

    update

---
+++ index.js
@@ -0,0 +1,56 @@
+export const translationsEU = {
+  company: 'Law Company 911',
+  homeHeader: 'Home',
+  aboutHeader: 'About Us',
+  practiceHeader: 'Practice Area',
+  blogHeader: 'Blog',
+  contactHeader: 'Contact us',
+  get: 'GET LEGAL SUPPORT',
+  provide: 'We Provide The Best Possible',
+  service: 'Legal Services',
+  headtext:
+    'Our team of experienced lawyers and legal experts is ready to help you to solve the most difficult legal issues. We offer individual approach to each client and provide full privacy',
+  button: 'Contact us',
+  practice: 'Our Practice Areas',
+  practicetext:
+    'We provide professional assistance in various fields of law, ensuring the protection of your rights and interests. Our lawyers have many years of experience and are ready to help you in any legal situation.',
+  criminal: 'Criminal Law',
+  civil: 'Civil Law',
+  business: 'Business law',
+  advocates: 'We’re Advocates for Justice and Right',
+  believe:
+    'We believe that justice and law should be available to everyone. Our team works to ensure the effective protection of our clients interests.',
+  solved: 'Cases Solved',
+  happy: 'Happy Clients',
+  years: 'Years of experience',
+  consultations: 'Consultations every year',
+  let: 'Let Our Experience',
+  guide: 'Be Your Guide',
+  experience1:
+    'In a world of legal complexities, it is important to have a reliable partner who knows how to succeed. Our team of experts is ready to share with your years of experience so you can focus on yours goals, and not on legal obstacles.',
+  experience2: 'We offer',
+  experience3:
+    'Individual approach: Each client is unique, so we develop personalized solutions that meet your needs.',
+  experience4:
+    'Deep Knowledge: Our team has deep knowledge in various branches of law, which allows us to effectively solve even the most difficult situations.',
+  experience5:
+    'Transparency and Trust: We believe in openness and honesty key elements in customer relations. You will always be in the course of the case and you will receive clear information.',
+  experience6:
+    'Support every step of the way: Whether you need it or not consultation, representation in court or assistance in running a business, we will be with you every step of the way.',
+  experience7:
+    'Choosing our firm, you get not only legal support, but also a reliable partner ready to fight for your interests.',
+
+  clients: 'What Our Clients Are Saying',
+  materials: 'Blog Posts',
+  materialsFirst: 'How to choose a lawyer: key points for clients',
+  materialsSecond:
+    'Rights and responsibilities of employees and employers: what you need to know about employment contracts',
+  materialsTherd:
+    'What to do in case of a family dispute: preservation of rights in marriage and divorce',
+  gallery: 'Gallery',
+  footerText:
+    'Our team is ready to answer your questions and provide a professional  help at any time convenient for you. Contact us by phone, email or visit our office for personal consultation. We are always on your side and strive to find the best solutions for you.',
+  contactFooter: 'Contact Now',
+  adress:
+    'Primorsky business center, str. Velika Arnautska, 17, Odesa, Ukraine',
+};
--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper-container', {
+const swiperGalerry = new Swiper('.swiper-container', {
   loop: true, // Зацикливание слайдов
   navigation: {
     nextEl: '.swiper-button-next',
@@ -14,3 +14,25 @@ const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1, // Сколько слайдов показывать одновременно
   spaceBetween: 30, // Расстояние между слайдами (в пикселях)
 });
+
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
